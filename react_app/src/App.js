81468d479ee615c736b29de70589dd0bf523b64a
jashwant1234 <jpradhan727@gmail.com>
2021-04-11 17:41:47 +0530

    passing Method Reference Between Components

--- react_app/src/App.js
+++ react_app/src/App.js
@@ -1,27 +1,45 @@
-import React, { Component } from 'react';
-import './App.css';
-import Person from './Person/Person';
+import React, { Component } from "react";
+import "./App.css";
+import Person from "./Person/Person";
 
 class App extends Component {
   state = {
-    persons : [
-      {name : "ashuthos" , age : "20"},
-      {name : "sambit" , age : "22"},
-      {name : "jashwant" , age : "21"}
-    ]
-  }
- switchNameHandler = () => {
-   console.log("jashwant");
- }
+    persons: [
+      { name: "ashuthos", age: "20" },
+      { name: "sambit", age: "22" },
+      { name: "jashwant", age: "21" },
+    ],
+  };
+  switchNameHandler = (newName) => {
+    this.setState({
+      persons: [
+        { name: newName, age: "20" },
+        { name: "sambit!", age: "22" },
+        { name: "jashwant!", age: "21" },
+      ],
+    });
+  };
 
-  render(){
+  render() {
     return (
       <div className="App">
         <h1>Hi i am Jaswwant.</h1>
-        <button onClick = {this.switchNameHandler}>Click</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}>I love to play criciket .</Person>
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+        <button onClick={() =>  this.switchNameHandler("somu!!!!")}>Click</button>
+        <Person
+          name={this.state.persons[0].name}g
+          age={this.state.persons[0].age}
+          click={this.switchNameHandler.bind(this,"jashwant!!!!")}
+        >
+          I love to play criciket .
+        </Person>
+        <Person
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}
+        />
+        <Person
+          name={this.state.persons[2].name}
+          age={this.state.persons[2].age}
+        />
       </div>
     );
   }
--- react_app/src/Person/Person.js
+++ react_app/src/Person/Person.js
@@ -3,7 +3,7 @@ import React from 'react';
 const person = (props) => {
     return (
     <div>
-        <p>My name is {props.name} and I am {props.age} Year old.</p>
+        <p onClick={props.click}>My name is {props.name} and I am {props.age} Year old.</p>
         <p>{props.children}</p>
     </div>
     );
