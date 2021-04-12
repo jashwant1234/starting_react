905758e9b9f82e80f462e06d892485bfadf9dd17
jashwant1234 <jpradhan727@gmail.com>
2021-04-12 10:26:19 +0530

    add some condition to react app

--- react_app/src/App.js
+++ react_app/src/App.js
@@ -9,6 +9,7 @@ class App extends Component {
       { name: "sambit", age: "22" },
       { name: "jashwant", age: "21" },
     ],
+    showPersons : false,
   };
   switchNameHandler = (newName) => {
     this.setState({
@@ -29,28 +30,37 @@ class App extends Component {
       ],
     });
   };
+  togglePerson = () =>{
+    this.setState({
+      showPersons : ! this.state.showPersons
+    })
+  }
 
   render() {
     return (
       <div className="App">
         <h1>Hi i am Jaswwant.</h1>
-        <button onClick={() =>  this.switchNameHandler("somu!!!!")}>Click</button>
-        <Person
-          name={this.state.persons[0].name}
-          age={this.state.persons[0].age}
-          click={this.switchNameHandler.bind(this,"jashwant!!!!")}
-          change = {this.nameChangeHandler}
-        >
-          I love to play criciket .
-        </Person>
-        <Person
-          name={this.state.persons[1].name}
-          age={this.state.persons[  1].age}
-        />
-        <Person
-          name={this.state.persons[2].name}
-          age={this.state.persons[2].age}
-        />
+        <button onClick={this.togglePerson}>Show Person</button>
+        { this.state.showPersons ? 
+        <div>
+          <Person
+            name={this.state.persons[0].name}
+            age={this.state.persons[0].age}
+            click={this.switchNameHandler.bind(this,"jashwant!!!!")}
+            change = {this.nameChangeHandler}
+          >
+            I love to play criciket .
+          </Person>
+          <Person
+            name={this.state.persons[1].name}
+            age={this.state.persons[  1].age}
+          />
+          <Person
+            name={this.state.persons[2].name}
+            age={this.state.persons[2].age}
+          />
+        </div> : null 
+        }
       </div>
     );
   }
