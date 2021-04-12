9aa8c1dfeb3f2745429b40f555cb93b2e867570a
jashwant1234 <jpradhan727@gmail.com>
2021-04-12 10:43:48 +0530

    add map to persons rendering

--- react_app/src/App.js
+++ react_app/src/App.js
@@ -43,22 +43,9 @@ class App extends Component {
     if(this.state.showPersons){
       person = (
         <div>
-        <Person
-          name={this.state.persons[0].name}
-          age={this.state.persons[0].age}
-          click={this.switchNameHandler.bind(this,"jashwant!!!!")}
-          change = {this.nameChangeHandler}
-        >
-          I love to play criciket .
-        </Person>
-        <Person
-          name={this.state.persons[1].name}
-          age={this.state.persons[  1].age}
-        />
-        <Person
-          name={this.state.persons[2].name}
-          age={this.state.persons[2].age}
-        />
+          {this.state.persons.map(person => {
+            return <Person name={person.name} age={person.age} />
+          }) }
       </div>
       )
     }
