83912202ec8677e1c6a3b2c9e3e59151f1038ca6
jashwant1234 <jpradhan727@gmail.com>
2021-04-27 19:13:06 +0530

    add http req repo and work in http get requst

---
+++ http_requst_in_react/src/containers/Blog/Blog.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import Post from '../../components/Post/Post';
+import FullPost from '../../components/FullPost/FullPost';
+import NewPost from '../../components/NewPost/NewPost';
+import './Blog.css';
+
+class Blog extends Component {
+    state = {
+        posts:[],
+    }
+
+    componentDidMount() {
+        axios.get('http://jsonplaceholder.typicode.com/posts')
+        .then(res => {
+            this.setState({posts: res.data})
+            console.log(res);
+        })
+    };
+
+    render () {
+        const posts = this.state.posts.map(post => {return ( <Post key={post.id} title={post.title}/>)})
+        return (
+            <div>
+                <section className="Posts">
+                   {posts}
+                </section>
+                <section>
+                    <FullPost />
+                </section>
+                <section>
+                    <NewPost />
+                </section>
+            </div>
+        );
+    }
+}
+
+export default Blog;
