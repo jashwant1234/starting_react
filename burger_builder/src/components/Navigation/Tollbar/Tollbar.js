162980451ea1f42461f77cab3c526e5a66ec3c86
jashwant1234 <jpradhan727@gmail.com>
2021-04-27 10:11:50 +0530

    add nav-bar to burger builder

--- burger_builder/src/components/Navigation/Tollbar/Tollbar.js
+++ burger_builder/src/components/Navigation/Tollbar/Tollbar.js
@@ -20,9 +20,9 @@ const Tollbar = (props) => {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="mr-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+            <Nav.Link href="#home" active={true}>Burger Builder</Nav.Link>
+            <Nav.Link href="#link">Checkout</Nav.Link>
+            {/* <NavDropdown title="Dropdown" id="basic-nav-dropdown">
               <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">
                 Another action
@@ -32,7 +32,7 @@ const Tollbar = (props) => {
               <NavDropdown.Item href="#action/3.4">
                 Separated link
               </NavDropdown.Item>
-            </NavDropdown>
+            </NavDropdown> */}
           </Nav>
         </Navbar.Collapse>
       </Navbar>
