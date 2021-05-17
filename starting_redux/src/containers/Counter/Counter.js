c43e20a8cc4466c2c6f9887276851fff7eeb283a
jashwant1234 <jpradhan727@gmail.com>
2021-05-17 18:06:26 +0530

    add delete to reducer

--- starting_redux/src/containers/Counter/Counter.js
+++ starting_redux/src/containers/Counter/Counter.js
@@ -29,21 +29,39 @@ class Counter extends Component {
         return (
             <div>
                 <CounterOutput value={this.props.counter} />
-                <CounterControl label="Increment" clicked={() => this.counterChangedHandler( 'inc' )} />
-                <CounterControl label="Decrement" clicked={() => this.counterChangedHandler( 'dec' )}  />
-                <CounterControl label="Add 5" clicked={() => this.counterChangedHandler( 'add', 5 )}  />
-                <CounterControl label="Subtract 5" clicked={() => this.counterChangedHandler( 'sub', 5 )}  />
+                <CounterControl label="Increment" clicked={this.props.onIncrementCounter} />
+                <CounterControl label="Decrement" clicked={this.props.onDecrementCounter} />
+                <CounterControl label="Add 5" clicked={this.props.onAddCounter} />
+                <CounterControl label="Subtract 5" clicked={this.props.onSubtractCounter}/>
+                <hr></hr>
+                <button onClick={this.props.onResultButtonClick}>store result</button>   
+                <ul>
+                    {this.props.results.map((result)=>(
+                      <li key={result.id} onClick={() => {this.props.onDeleteResultButtonClick(result.id)}}>{result.value}</li>
+                    ))}
+                </ul>
             </div>
+           
         );
     }
 }
 
 const mapStateToProps = (state) =>{
     return {
-        counter: state.counter
-    }
+        counter: state.counter,
+        results: state.results
+    } 
 }
 
-const map
+const mapDispatchToProps = (dispatch) =>{ 
+    return { 
+        onIncrementCounter : () => dispatch({type:'INCREMENT'}),
+        onDecrementCounter : () => dispatch({type:'DECREMENT'}),
+        onAddCounter : () => dispatch({type:'ADD',value:5}),
+        onSubtractCounter : () => dispatch({type:'SUBTRACT',value:5}),
+        onResultButtonClick: () => dispatch({type:'RESULT'}),
+        onDeleteResultButtonClick: (id) => dispatch({type:'DELETE_RESULT', resultId: id})
+    };
+}
 
-export default connect(mapStateToProps)(Counter);
+export default connect(mapStateToProps,mapDispatchToProps)(Counter);
--- starting_redux/src/store/reducer.js
+++ starting_redux/src/store/reducer.js
@@ -1,9 +1,43 @@
 const initialState = {
-    counter : 10,
-}
+  counter: 10,
+  results: [],
+};
 
 const reducer = (state = initialState, action) => {
-    return state;
-}
+  switch (action.type) {
+    case "INCREMENT":
+      return {
+          ...state,
+        counter: state.counter + 1,
+      };
+    case "DECREMENT":
+      return {
+        ...state,
+        counter: state.counter - 1,
+      };
+    case "ADD":
+      return {
+        ...state,
+        counter: state.counter + action.value,
+      };
+    case "SUBTRACT":
+      return {
+        ...state,
+        counter: state.counter - action.value,
+      };
+      case "RESULT":
+      return {
+        ...state,
+        results: state.results.concat({id: new Date,value:state.counter})
+      };
+      case "DELETE_RESULT":
+      const newArray = state.results.filter(result => (result.id !== action.resultId));
+      return {
+        ...state,
+        results:newArray
+      };
+  }
+  return state;
+};
 
 export default reducer;
